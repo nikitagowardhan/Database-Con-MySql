b7229d98372745e0fee44a16f44387a578438b40
nikitagowardhan <nikitagowar02@gmail.com>
2024-01-30 14:13:14 +0530

    Database Connection to Servlet

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,9 +1,7 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager; 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,48 +13,40 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class MyServlet extends HttpServlet {
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		response.setContentType("text/html");
-		PrintWriter p = response.getWriter();
-		
-		String uname = request.getParameter("name");
-		String upass = request.getParameter("upass");
-		String uemailid = request.getParameter("emailid");
-		String gender = request.getParameter("gender");
-        String ueligibility = request.getParameter("ueligibility");
-		
-		p.print("hello");
-		p.print("hello "+uname);
-			
-	
-		
-		 try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "abc123");
-	            Statement s = c.createStatement();
 
-	            String query = "INSERT INTO db_table (Name, Password, Emailid, Gender, Eligibility) VALUES " +
-	                           "('" + uname + "','" + upass + "','" + uemailid + "','" + gender + "','" + ueligibility + "')";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	            int a = s.executeUpdate(query);
+        response.setContentType("text/html");
+        PrintWriter p = response.getWriter();
 
-	            if (a > 0) 
-	            {
-	                p.print("DATA INSERTED");
-	            } else {
-	                p.print("DATA NOT INSERTED");
-	            }
+        String uname = request.getParameter("name");
+        String upass = request.getParameter("upass");
+        String uemailid = request.getParameter("emailid");
+        
 
-	            
-	            s.close();
-	            c.close();
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "root", "abc123");
+            Statement statement = c.createStatement();
 
-	        } catch (ClassNotFoundException | SQLException e) {
-	            e.printStackTrace();
-	            p.print("DATABASE ERROR");
-	        }
-	}
+            String query = "INSERT INTO my_table (name, password, emailid) VALUES " +
+                           "('" + uname + "','" + upass + "','" + uemailid + "')";
 
+            int a = statement.executeUpdate(query);
+
+            if (a > 0) {
+                p.print("DATA INSERTED");
+            } else {
+                p.print("DATA NOT INSERTED" );
+            }
+
+            statement.close();
+            c.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            p.print("DATABASE ERROR: " + e.getMessage());        }
+    }
 }
